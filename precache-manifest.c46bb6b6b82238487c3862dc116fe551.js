f2199a6e106986336c8c42fe4d8c905710537bcb
githubaction@fake.com <githubaction>
2020-12-26 10:51:40 +0000

    deploy to Github pages

---
+++ precache-manifest.c46bb6b6b82238487c3862dc116fe551.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58c5167934d04701131",
+    "url": "hello-actions/css/app.f18657ad.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "hello-actions/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0c07a3a4043c9c5f9a0432e36245c264",
+    "url": "hello-actions/index.html"
+  },
+  {
+    "revision": "0d6e826cd3dc209395b1",
+    "url": "hello-actions/js/about.55c98ffd.js"
+  },
+  {
+    "revision": "d58c5167934d04701131",
+    "url": "hello-actions/js/app.6cc50027.js"
+  },
+  {
+    "revision": "c64f5ee37f9a754c2e3f",
+    "url": "hello-actions/js/chunk-vendors.e19f14a0.js"
+  },
+  {
+    "revision": "f3c8e159b2b50e28cba86cb22cf10e0f",
+    "url": "hello-actions/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "hello-actions/robots.txt"
+  }
+]);
